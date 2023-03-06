a2eb6a4d0bcf07143520a33827084915f0ac90d3
zx1000711 <1203771565@qq.com>
2023-03-06 16:52:50 +0800

    test

--- test/src/main/java/com/zx/demo/service/Testservice.java
+++ test/src/main/java/com/zx/demo/service/Testservice.java
@@ -5,6 +5,19 @@ package com.zx.demo.service;
  * @date 2023/3/1 18:04
  */
 public class Testservice {
-    
 
+    public static void main(String[] args){
+        Thread aa = new Thread(()->{
+            System.out.println(Thread.currentThread().getName() +"::"+ Thread.currentThread().isDaemon());
+            while(true){ }
+        }, "aa");
+        aa.setDaemon(true);
+        aa.start();
+        System.out.println(Thread.currentThread().getName()+"over");
+/*        new Thread(()->{
+            System.out.println(Thread.currentThread().getName() +"::"+ Thread.currentThread().isDaemon());
+            while(true){ }
+        }, "aa").start();
+        System.out.println(Thread.currentThread().getName()+"over");*/
+    }
 }
